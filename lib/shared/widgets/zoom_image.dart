431588982b57b52dfa6c36ba8be3680b9ebd25fc
Muflih-uk <muhammedmuflih875@gmail.com>
2026-06-04 14:35:27 +0530

    feat: create widget for zoom the image

---
+++ lib/shared/widgets/zoom_image.dart
@@ -0,0 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ZoomableImage extends StatelessWidget {
+  final String imageUrl;
+  final double width;
+  final double height;
+
+  const ZoomableImage({
+    super.key,
+    required this.imageUrl,
+    this.width = 55,
+    this.height = 55,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => FullScreenImageViewer(imageUrl: imageUrl),
+          ),
+        );
+      },
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        width: width,
+        height: height,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class FullScreenImageViewer extends StatelessWidget {
+  final String imageUrl;
+
+  const FullScreenImageViewer({super.key, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+
+      body: Center(
+        child: InteractiveViewer(
+          minScale: 1,
+          maxScale: 5,
+          child: CachedNetworkImage(imageUrl: imageUrl, fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
