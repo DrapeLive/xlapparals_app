1972faccd19a2af10b79e603d3ceea9ca01f48a9
Muflih-uk <muhammedmuflih875@gmail.com>
2026-06-13 11:50:24 +0530

    config: register thr blocs

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,14 @@ import 'package:xlapparals_app/features/agent/home/presentation/blocs/bottom_nav
 import 'package:xlapparals_app/features/agent/home/presentation/blocs/items/expand_item/item_bloc.dart';
 import 'package:xlapparals_app/features/agent/home/presentation/blocs/items/fetch_item/item_fetch_bloc.dart';
 import 'package:xlapparals_app/features/agent/home/presentation/blocs/orders/orders_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/customers/presentation/blocs/customer_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/customers/presentation/blocs/customer_event.dart';
+import 'package:xlapparals_app/features/agent/orders/customers/presentation/blocs/order_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/order_details/presentation/blocs/agent_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/order_details/presentation/blocs/order_fetch_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/order_items/presentation/blocs/item_details_bloc.dart';
+import 'package:xlapparals_app/features/agent/orders/scanner/presentation/blocs/scan_bloc.dart';
+import 'package:xlapparals_app/features/agent/profile/presentation/blocs/profile/profile_bloc.dart';
 
 import 'core/routes/app_router.dart';
 import 'core/theme/app_theme.dart';
@@ -24,6 +32,13 @@ class App extends StatelessWidget {
         BlocProvider(create: (_) => BottomNavBloc()),
         BlocProvider<ItemFetchBloc>(create: (_) => sl<ItemFetchBloc>()),
         BlocProvider(create: (_) => ItemBloc()),
+        BlocProvider<ProfileBloc>(create: (_) => sl<ProfileBloc>()),
+        BlocProvider(create: (_) => sl<CustomerBloc>()..add(FetchCustomers())),
+        BlocProvider(create: (_) => sl<OrderDetailsBloc>()),
+        BlocProvider(create: (_) => sl<CreateOrderBloc>()),
+        BlocProvider(create: (_) => sl<ScanBloc>()),
+        BlocProvider(create: (_) => sl<ItemDetailsBloc>()),
+        BlocProvider(create: (_) => sl<AgentBloc>()),
       ],
       child: MaterialApp.router(
         title: AppConstants.appName,
