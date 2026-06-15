c295f4adf65a29de6f7bb295284f13d13b382d4d
Muflih-uk <muhammedmuflih875@gmail.com>
2026-06-15 11:27:48 +0530

    feat: add functionality for sign out button

--- lib/features/agent/profile/presentation/widgets/signout_button.dart
+++ lib/features/agent/profile/presentation/widgets/signout_button.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:xlapparals_app/core/constants/app_constants.dart';
+import 'package:xlapparals_app/core/routes/route_name.dart';
 import 'package:xlapparals_app/core/theme/app_colors.dart';
+import 'package:xlapparals_app/shared/services/secure_storage_service.dart';
 
 class SignOutButton extends StatelessWidget {
-  const SignOutButton({super.key});
+  SignOutButton({super.key});
+
+  final SecureStorageService storage = SecureStorageService();
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-      onPressed: () {},
+      onPressed: () async {
+        await storage.clear();
+        context.go(RouteNames.login);
+      },
       icon: const Icon(Icons.logout, color: Colors.red, size: 20),
       label: const Text(
         "Sign Out",
